49e7f18d0e2a2af0684e0639f42104c895924ca1
jercilla <joseba.ercilla@gmail.com>
2024-05-17 08:27:30 +0200

    refactor

--- lib.js
+++ lib.js
@@ -3,33 +3,36 @@
     +joseba.ercilla@gailen.es
 */
 function check(given, fizzBuzz, expected) {
-  let actual = fizzBuzz(given)
-console.log(actual)
-  let message = `Dado ${given}, cuando calculamos su fizzBuzz, obtendremos ${expected}`
-  let outputNode = document.querySelector('#output')
-  let className = 'test-ok'
-  let testMessage = ''
+  let actual = fizzBuzz(given);
+  let message = `Dado ${given}, cuando calculamos su fizzBuzz, obtendremos ${expected}`;
+  let outputNode = document.querySelector("#output");
+  let className = "test-ok";
+  let testMessage = "";
   if (actual !== expected) {
-    className = 'test-fail'
-    testMessage = `\n              Se esperaba ${expected}, pero el resultado es ${actual}`
+    className = "test-fail";
+    testMessage = `\n              Se esperaba ${expected}, pero el resultado es ${actual}`;
   }
-  outputNode.innerHTML += `<div class="${className}">${message}</div>`
-  outputNode.innerHTML += `<div class="test-message">${testMessage}</div>`
+  outputNode.innerHTML += `<div class="${className}">${message}</div>`;
+  outputNode.innerHTML += `<div class="test-message">${testMessage}</div>`;
+}
+
+function handleClick(input) {
+  document.getElementById("output").innerHTML = fizzBuzz(input.value);
 }
 
 function clearOutput() {
-  document.querySelector('#output').innerHTML = ''
+  document.querySelector("#output").innerHTML = "";
 }
 
 function main() {
-  let outputNode = document.querySelector('#output')
-  let output = ''
+  let outputNode = document.querySelector("#output");
+  let output = "";
 
-  output += `<ol>`
+  output += `<ol>`;
   for (let i = 1; i <= 100; i++) {
-    output += `<li>${fizzBuzz(i)}</li>`
+    output += `<li>${fizzBuzz(i)}</li>`;
   }
 
-  output += `</ol>`
-  outputNode.innerHTML = output
+  output += `</ol>`;
+  outputNode.innerHTML = output;
 }
